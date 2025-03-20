53fc04b2d01a96f854660d36792eb2522a6116e9
nandhana90 <nandhana3905@gmail.com>
2025-03-20 16:42:15 +0530

    Deployment

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    fontFamily:{
+      fonts:[ 'Poppins', 'sans-serif'],
+    },
+    fontWeight:{
+      bold:'500',
+      extra:'600',
+
+
+    },
+    colors:{
+       bag:'#800080',
+       text:'#FFFFFF',
+       bagc:'#CBC3E3',
+       liv:'#CF9FFF',
+    },
+    
+  },
+  plugins: [],
+}
+
